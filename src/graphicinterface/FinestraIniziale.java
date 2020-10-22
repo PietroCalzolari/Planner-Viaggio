4b95fe4ab90e913d738138c68cd30ca430e77e9d
Pietro <pietrocalzolari@live.com>
2020-10-22 17:20:21 +0200

    Ho migliorato la finestrainiziale mettendo i bordi giusti

--- src/graphicinterface/FinestraIniziale.java
+++ src/graphicinterface/FinestraIniziale.java
@@ -10,9 +10,14 @@ import javax.swing.JTextField;
 import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.UIManager;
 
-public class FinestraIniziale extends JPanel implements ActionListener{
+public class FinestraIniziale extends JFrame implements ActionListener{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JTextField textFieldNome;
 	private JTextField textField;
 	private JTextField textField_1;
@@ -22,51 +27,54 @@ public class FinestraIniziale extends JPanel implements ActionListener{
 	 * Create the panel.
 	 */
 	public FinestraIniziale() {
+		getContentPane().setBackground(new Color(255, 140, 0));
 		setBackground(new Color(244, 164, 96));
 		setForeground(Color.BLACK);
-		setLayout(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 586, 422);
+		getContentPane().setLayout(null);
 		
 		JLabel lblInserisciDati = new JLabel("INSERISCI I TUOI DATI");
 		lblInserisciDati.setFont(new Font("Comic Sans MS", Font.BOLD, 17));
 		lblInserisciDati.setBounds(142, 11, 240, 67);
-		add(lblInserisciDati);
+		getContentPane().add(lblInserisciDati);
 		
 		JLabel lblNome = new JLabel("Nome");
 		lblNome.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
 		lblNome.setBounds(135, 114, 54, 24);
-		add(lblNome);
+		getContentPane().add(lblNome);
 		
 		JLabel lblCognome = new JLabel("Cognome");
 		lblCognome.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
 		lblCognome.setBounds(135, 164, 69, 24);
-		add(lblCognome);
+		getContentPane().add(lblCognome);
 		
 		JLabel lblEmail = new JLabel("Email");
 		lblEmail.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
 		lblEmail.setBounds(135, 213, 54, 24);
-		add(lblEmail);
+		getContentPane().add(lblEmail);
 		
 		textFieldNome = new JTextField();
 		textFieldNome.setBounds(229, 118, 118, 20);
-		add(textFieldNome);
+		getContentPane().add(textFieldNome);
 		textFieldNome.setColumns(10);
 		
 		textField = new JTextField();
 		textField.setColumns(10);
 		textField.setBounds(229, 168, 118, 20);
-		add(textField);
+		getContentPane().add(textField);
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
 		textField_1.setBounds(229, 217, 118, 20);
-		add(textField_1);
+		getContentPane().add(textField_1);
 		
 		btnPrimoViaggio = new JButton("Aggiungi il tuo primo Viaggio!");
 		btnPrimoViaggio.addActionListener(this);
 		btnPrimoViaggio.setForeground(Color.BLACK);
 		btnPrimoViaggio.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
-		btnPrimoViaggio.setBounds(142, 266, 240, 53);
-		add(btnPrimoViaggio);
+		btnPrimoViaggio.setBounds(165, 263, 240, 53);
+		getContentPane().add(btnPrimoViaggio);
 
 	}
 
